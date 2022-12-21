8fd61e2d0ebe7039f5af11288ecd362d3007860d
nahuel@chipax.com <nahuel@chipax.com>
2022-12-21 09:30:21 -0300

    feat: agrega cors

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { FloidApiController } from './api/api.controller';
-
+var cors = require('cors');
 dotenv.config()
 
 const app: Express = express()
 const port = process.env.PORT
+
+app.use(cors());
 app.use(express.json())
 app.post('/get', async (req: Request, res: Response) => {
   const FloidApi = new FloidApiController()
